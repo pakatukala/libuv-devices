059e7fe8b3199d3abcca6d2a15726fb0fe1fbb9e
Pavan Katukala <pakatukala@icloud.com>
2023-06-13 16:40:57 -0400

    Initial commit

---
+++ src/test/java/com/device/application/deviceapplication/DeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.device.application.deviceapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
